051d98da000f1f98433421e30374fa7fc5c9a4a3
ch-hi <73207820+ch-hi@users.noreply.github.com>
2020-12-25 02:20:18 +0900

    バリデーションチェックの追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,7 +10,8 @@ class ArticlesController < ApplicationController
   end
 
   # 記事の作成
-  def new 
+  def new
+    @article = Article.new
   end
 
   # 記事の登録
@@ -18,13 +19,16 @@ class ArticlesController < ApplicationController
     # articleモデルの属性の初期化
     @article = Article.new(article_params)
 
-    # articleモデルをDBへ保存
-    @article.save
-
-    # showアクションにリダイレクト
-    redirect_to @article
+      # DBに登録できた場合
+      if @article.save
+        #記事ページに遷移する
+        redirect_to @article
+      # DBに登録できなかった場合
+      else
+        #作成ページに遷移する
+        render 'new'
+      end
   end
-
   # 記事の編集
   def edit
   end
